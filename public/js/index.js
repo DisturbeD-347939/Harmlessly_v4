704eb874711618883aa99a4d53112bfbc8ff1431
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-28 13:50:33 +0000

    Register form handling

--- public/js/index.js
+++ public/js/index.js
@@ -49,7 +49,61 @@ $(document).ready(function()
     $('#registerForm').submit(function(e)
     {
         e.preventDefault();
-        console.log("Submitted register");
+
+        var valid = true;
+        var inputs = $('#registerForm :input');
+        var values = {};
+        inputs.each(function()
+        {
+            values[this.name] = $(this).val();
+        })
+
+        $('#registerForm > div > input').each(function(index)
+        {
+            if(!$(this).hasClass("valid")) { valid = false; }
+        })
+
+        if(valid)
+        {
+            $.post("/register",
+            {
+                data: values
+            },
+            function(data, status)
+            {
+                //200 - OK
+                if(data == "200")
+                {
+                    $('.back').click();
+                    alert("Account created!");
+                    $('#registerForm > div > input').val("");
+                }
+    
+                //409 - Email in use
+                if(data == "409")
+                {
+                    alert("Email already in use, try again!");
+                    $('#registerForm > div:nth-child(2) > input').val("");
+                    $('#registerForm > div:nth-child(2) > input').removeClass("valid");
+                    $('#registerForm > div:nth-child(2) > input').addClass("invalid");
+                }
+    
+                //500- Documents not found
+                if(data == "500")
+                {
+                    $('.back').click();
+                    alert("Sorry, we are having trouble with our servers at the moment. Try again later!");
+                }
+    
+            })
+            console.log("Submitted register");
+        }
+        else
+        {
+            alert("Fill up all the fields correctly before submitting!");
+        }
+    })
+
     //Enable users to press enter to submit the form
     $("#registerForm").keypress(function(event)
     {
