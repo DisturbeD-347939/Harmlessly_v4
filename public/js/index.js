e7e759164e75b9fde7720e0efe30eb7051b7c37d
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-28 15:38:00 +0000

    Moved service worker

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,14 @@
+//Check if browser supports service workers.
+if('serviceWorker' in navigator)
+{
+    //Put it in all scripts (installs/updates the service worker)
+    navigator.serviceWorker.register('/sw.js')
+    .then(function()
+    {
+        console.log("Service worked registered!");
+    })
+}
+
 $(document).ready(function()
 {
     $('#loginDiv').hide();
