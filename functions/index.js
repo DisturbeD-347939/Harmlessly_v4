3b3ff6257c100232af0026e18512770967767d86
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-28 13:47:00 +0000

    Added login and register functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,16 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const engines = require('consolidate');
+var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var express = require('express');
+var engines = require('consolidate');
+var bcrypt = require('bcryptjs');
+var serviceAccount = require('./ServiceAccountKey.json');
+
+admin.initializeApp
+({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 const app = express();
 
@@ -27,6 +37,90 @@ app.get('/home', (request, response) =>
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
     //Render page
     response.render('home');
+})
+
+app.post('/register', (request, response) =>
+{
+    var data = request.body.data;
+    var emailAvailable = true;
+    var counter = 0;
+
+    db.collection('Users').get()
+    .then(snapshot => 
+    {
+        snapshot.forEach(doc => 
+        {
+            if(doc.id == data.email)
+            {
+                console.log("Same email");
+                emailAvailable = false;
+            }
+            counter++;
+            if(counter == snapshot["_size"])
+            {
+                bcrypt.genSalt(10, function(err, salt)
+                {
+                    bcrypt.hash(data["password"], salt, function(err, hash) 
+                    {
+                        var fields = 
+                        {
+                            username: data.name,
+                            password: hash
+                        }
+                        if(emailAvailable)
+                        {
+                            db.collection('Users').doc(data.email).set(fields).then(() =>
+                            {
+                                console.log("Account created");
+                                response.send("200");
+                            })
+                        }
+                        else
+                        {
+                            console.log("Email in use");
+                            response.send("409");
+                        }
+                    });
+                });
+            }
+        })
+    })
+    .catch(err => 
+    {
+      console.log('Error getting documents', err);
+      response.send("500");
+    });
+})
+
+app.post('/login', (request, response) =>
+{
+    var data = request.body.data;
+    
+    db.collection('Users').doc(data.email).get()
+    .then(function(doc) 
+    {
+        if (doc.exists) 
+        {
+            bcrypt.compare(data.password, doc.data()["password"], function(err, res) 
+            {
+                if(res == true)
+                {
+                    console.log("Logged in");
+                    response.render('home', {email: data.email});
+                }
+                else
+                {
+                    console.log("Wrong email/password");
+                    response.send("409");
+                }
+            });
+        } 
+        else 
+        {
+            console.log("No such document!");
+            response.send("500");
+        }
+    })
 })
 
 //Export app
