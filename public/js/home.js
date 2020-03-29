941e1cea348170d16b3b686ae408b5d6309c7b3f
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-29 16:36:19 +0100

    Setup part where elements are adjusted before being displayed

--- public/js/home.js
+++ public/js/home.js
@@ -20,6 +20,32 @@ function getCookie(cname)
 
 $(document).ready(function()
 {
+    /***********************************************SETUP**********************************************/
+    
+    //Hide elements
+    $('#addUse').hide();
+
+    //Set content height and position
+    var remainingScreen = ($("body").height() - ($("header").height() + 10 + $("footer").height() + 10));
+    var percentageFreeScreen = Math.round(((remainingScreen * 100)/$("body").height())) + "%";
+    $(".content").height(percentageFreeScreen);
+    $(".content").css("top", $("header").height() + 10);
+
+    //Setup add tab bottom
+    $('#addUse').css("bottom", $("footer").height() + 15 + "px");
+
+    //Get drug info
+    $.get("/info", function(data, status)
+    {
+        var substances = Object.keys(data.Substances);
+        
+
+        for(var i = 0; i < substances.length; i++)
+        {
+            $("#substancesSelect").append("<option value= " + substances[i] + ">" + substances[i] + "</option>")
+        }
+    });
+
     
     $('#add').click(function()
     {
